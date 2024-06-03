d2f31c1435887b5d381ffeb013ab0d4f69a65397
Giovanni Ricca <giovanniricca@proton.me>
2024-06-03 08:40:20 +0200

    key-migration: Match `migration.sh` vars order
    Change-Id: I69007c0c1b41492832069fbf55103d21d01e06e5

--- key-migration/export-keys.sh
+++ key-migration/export-keys.sh
@@ -8,10 +8,10 @@ if ! cd "$1"; then
     exit 1
 fi
 
-for x in platform media shared; do
-    echo ${x}_key_release=\"$(openssl x509 -pubkey -noout -in $x.x509.pem | grep -v '-' | tr -d '\n')\"
+for x in media platform shared; do
     echo ${x}_cert_release=\"$(openssl x509 -outform der -in $x.x509.pem | xxd -p  | tr -d '\n')\"
+    echo ${x}_key_release=\"$(openssl x509 -pubkey -noout -in $x.x509.pem | grep -v '-' | tr -d '\n')\"
 done
 
-echo release_key=\"$(openssl x509 -pubkey -noout -in releasekey.x509.pem | grep -v '-' | tr -d '\n')\"
 echo release_cert=\"$(openssl x509 -outform der -in releasekey.x509.pem | xxd -p  | tr -d '\n')\"
+echo release_key=\"$(openssl x509 -pubkey -noout -in releasekey.x509.pem | grep -v '-' | tr -d '\n')\"
