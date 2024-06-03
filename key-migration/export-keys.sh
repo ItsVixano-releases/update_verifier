7cb2823349311adb4646dd8c17ba154dd8a8e2de
Giovanni Ricca <giovanniricca@proton.me>
2024-06-03 08:40:20 +0200

    key-migration: Match `migration.sh` output
    Change-Id: I138115f920e311cf86a05c84222ea225d163940e

--- key-migration/export-keys.sh
+++ key-migration/export-keys.sh
@@ -4,14 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if ! cd "$1"; then
-    echo "USAGE: $0 PATH"
+    echo 'USAGE: $0 PATH'
     exit 1
 fi
 
-for x in platform media shared; do
-    echo ${x}_key_release=\"$(openssl x509 -pubkey -noout -in $x.x509.pem | grep -v '-' | tr -d '\n')\"
-    echo ${x}_cert_release=\"$(openssl x509 -outform der -in $x.x509.pem | xxd -p  | tr -d '\n')\"
+for x in media platform shared; do
+    echo ${x}_cert_release=\'$(openssl x509 -outform der -in $x.x509.pem | xxd -p  | tr -d '\n')\'
+    echo ${x}_key_release=\'$(openssl x509 -pubkey -noout -in $x.x509.pem | grep -v '-' | tr -d '\n')\'
 done
 
-echo release_key=\"$(openssl x509 -pubkey -noout -in releasekey.x509.pem | grep -v '-' | tr -d '\n')\"
-echo release_cert=\"$(openssl x509 -outform der -in releasekey.x509.pem | xxd -p  | tr -d '\n')\"
+echo release_cert=\'$(openssl x509 -outform der -in releasekey.x509.pem | xxd -p  | tr -d '\n')\'
+echo release_key=\'$(openssl x509 -pubkey -noout -in releasekey.x509.pem | grep -v '-' | tr -d '\n')\'
